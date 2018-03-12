89fddd8c5a7465b40ef7bdd0b21f1a809f3796ce
yn1046 <wantedblood@2ch.hk>
2018-03-12 12:47:40 +0300

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB5N5S3CbmgH8KLHZXTVYNXbgdNojWOB8M",
+    authDomain: "simplechat-d77f8.firebaseapp.com",
+    databaseURL: "https://simplechat-d77f8.firebaseio.com",
+    projectId: "simplechat-d77f8",
+    storageBucket: "simplechat-d77f8.appspot.com",
+    messagingSenderId: "590161814902"
+  }
 };
